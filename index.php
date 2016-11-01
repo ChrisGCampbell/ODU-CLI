d5d1720f9a05344c6877f9baa9c75a8017efdf5f
ChrisGCampbell <chris@chrisgcampbell.com>
2016-10-31 20:12:08 -0400

    added index.php file

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris-Campbell
+ * Date: 10/31/2016
+ * Time: 8:05 PM
+ */
+include 'header.html';
+?>
+
+<h4>Conservation Leadership Workspace</h4>
+
+<p><b>Important Action</b>: Please provide comments to Hans-Peter Plag and Eddie Hill on the draft research agenda survey available at <a href="https://odu.co1.qualtrics.com/jfe/form/SV_86TyvK62XlY1Iot">https://odu.co1.qualtrics.com/jfe/form/SV_86TyvK62XlY1Iot</a> and the curriculum survey available at <a href="https://odu.co1.qualtrics.com/jfe/form/SV_3RcB2H9Mb0WtUFL">https://odu.co1.qualtrics.com/jfe/form/SV_3RcB2H9Mb0WtUFL</a>. It is planned to invite faculty by August 18, 2016 to take the survey.
+
+<p>Here is the draft invitation text:
+<hr>
+<p>ODU's Mitigation and Adaptation Research Institute is engaged in a collaboration with the U.S. Fish and Wildlife Service (FWS) to facilitate the development of the next generation of conservation professionals with the goal to enable these professionals to address conservation under changing climate and rising sea levels. A new minor has been established and two certificates are under development.
+
+<p>To inform future curriculum development, a survey is being conducted to identify existing courses at ODU that are aligned with needs for new curricula preparing the next generation of leaders in the face of climate change and sea level rise. It also aims to assess the availability of faculty for the development of new courses where needed. We invite all faculty to take this survey.
+
+<p>One project objective is to co-design a research agenda to address the challenges climate change and sea level rise pose to conservation. To support this activity, we invite faculty engaged in research related to climate change, sea level rise, and/or conservation to take the survey informing the development of a research agenda for conservation under climate change.
+
+<p>Best regards,
+
+    ....
+<hr>
+
+
+<p>Please, use the "comment" utility to provide input for the various activities. If you would like to add an additional theme to the comment utility, please use the "Request Category" link in the menu.
+</td></tr></table>
+</div>
+
+<?php include 'footer.html'; ?>
