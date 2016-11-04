bcc023ffd98f698c034c86c00dfbd1ae5b1356a4
ChrisGCampbell <chris@chrisgcampbell.com>
2016-11-04 15:40:32 -0400

    added functionality to display full incident

--- add_ai.php
+++ add_ai.php
@@ -7,9 +7,15 @@ if(isset($_POST['submitGroup'])){
        $group = trim($_POST['group']);
        $incidents = displayIncident($group);
        for($j=1; $j<count($incidents); $j++){
-           echo '<button class="incidents">'.$incidents[$j].'</button><br/><br/>';
+           echo '<a name="incidentLookup" href="add_ai.php?groupname='.$group.'&incidentnumber='.$incidents[$j].'">'.$incidents[$j].'</a><br/><br/>';
        }
    }
+}
+else if(isset($_GET['incidentnumber'])){
+ $incidentNumber = $_GET['incidentnumber'];
+ $group = $_GET['groupname'];
+ echo $group . "<br/>";
+ echo $incidentNumber . "<br/>";
 }
 else {
     echo "<form id='groupList' method='POST' action='"; echo $_SERVER['PHP_SELF']; echo "'>";
