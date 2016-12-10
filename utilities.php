4734195c0357be7cafc36647f7fc493ecd19019d
ChrisGCampbell <chris@chrisgcampbell.com>
2016-12-10 11:53:52 -0500

    added custom Error Handler to utilities.php

--- utilities.php
+++ utilities.php
@@ -9,6 +9,7 @@ define("GROUPINCIDENTS", "DataFiles/known_incidents.txt");
 define("ACTIONITEMS", "DataFiles/Actionitems.xml");
 define("AIREPORTS", "DataFiles/aireports.xml");
 define("REGISTERED_USERS", "DataFiles/user_file.xml");
+error_reporting(0);
 
 class ActionReports {
     protected $_firstname;
@@ -26,19 +27,39 @@ class ActionReports {
     }
 
 
-    protected function displayGroupSelectionList() {
+    public function displayGroupSelectionList() {
         $groupNames = [];
         $fileHandler = fopen(GROUPFILE, "r");
         $groupCount = fgets($fileHandler);
 
-        for ($i = 0; $i < $groupCount; $i++) {
-            $groupNames[$i] = fgets($fileHandler);
+        try {
+            if (!$fileHandler) {
+                throw new customException("Failed to open groupfile on ");
+            }
+        }
+        catch(customException $e) {
+            echo $e->errorMessage();
         }
 
-        for ($i = 0; $i < count($groupNames); $i++) {
-            echo '<input type="radio" name="group" id="group" value="' . $groupNames[$i] . '">' . $groupNames[$i];
+
+        for ($i = 0; $i < $groupCount; $i++) {
+            echo '<input type="radio" name="group" id="group" value="'; echo  $groupNames[$i] = fgets($fileHandler); echo  '">' . $groupNames[$i];
         }
+
+        echo '&nbsp<input type="submit" value="submit" name="submitGroup"></form>';
     }
+
 }
 
 
+class customException extends Exception {
+    public function errorMessage() {
+        $currentTime = date('H:i, jS F Y');
+        $errorfile = fopen("DataFiles/error-file.txt", "a");
+        $errormessage = $this->getMessage() . " " . $currentTime;
+        fwrite($errorfile,$errormessage);
+        fclose($errorfile);
+        echo "An error has occurred. Please contact the system administror.";
+        exit();
+    }
+}
